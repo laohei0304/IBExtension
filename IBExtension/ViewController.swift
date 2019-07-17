af466f8774eaa653537431b776811e3f1d8f7ef5
laohei0304 <153310938@qq.com>
2019-07-17 10:47:10 +0800

    提交

---
+++ IBExtension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IBExtension
+//
+//  Created by liyang on 2019/7/17.
+//  Copyright © 2019 laohei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
