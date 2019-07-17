96da7c2c5f0eb6c025923fc41df3944ebf13899c
laohei0304 <153310938@qq.com>
2019-07-17 09:20:54 +0800

    IBDesignable扩展

---
+++ IBView.swift
@@ -0,0 +1,29 @@
+//
+//  IBView.swift
+//  IBExtension
+//
+//  Created by liyang on 2019/7/17.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class DBView:UIView {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
